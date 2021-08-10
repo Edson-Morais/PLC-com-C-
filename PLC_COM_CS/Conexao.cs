22cee972e19788f636d3ac885167aff1adc63628
Edson A. Morais <ed-news@hotmail.com>
2021-08-10 16:32:02 -0300

    criando classe conexao

---
+++ PLC_COM_CS/Conexao.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyModbus;
+
+namespace PLC_COM_CS
+{
+    class Conexao
+    {
+        private ModbusClient clp = new ModbusClient();
+        private int[] registros;
+        private bool[] coils;
+        public bool con;
+
+
+
+        // Construtor
+        public Conexao()
+        {
+            
+        }
+
+        // Metodo Conectar
+        private int[] lerRegistros( string ip,int inicio, int quantidade)
+        {
+            try
+            {
+                clp.Connect(ip, 502);
+                registros = clp.ReadHoldingRegisters(inicio, quantidade);
+                con = true;
+            }
+            catch
+            {
+                con = false;
+            }
+            return registros;
+        }
+        private bool[] lerCoils(string ip, int inicio, int quantidade)
+        {
+            try
+            {
+                clp.Connect(ip, 502);
+                coils = clp.ReadCoils(inicio, quantidade);
+                con = true;
+            }
+            catch
+            {
+                con = false;
+            }
+            return coils;
+        }
+
+    }
+}
---
+++ PLC_COM_CS/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace PLC_COM_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o IP...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(41, 114);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(279, 311);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
